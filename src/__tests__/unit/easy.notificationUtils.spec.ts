940ecc021cbde79f9dae55e7d11b1733a14270cc
seriparkdev <parkseridev@gmail.com>
2024-11-06 00:09:49 +0900

    test: notificationUtils util test 작성

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,16 +1,60 @@
+import { expect } from 'vitest';
+
 import { Event } from '../../types';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
+const events: Event[] = [
+  {
+    id: 'da3ca408-836a-4d98-b67a-ca389d07552b',
+    title: '프로젝트 마감',
+    date: '2024-10-25',
+    startTime: '09:00',
+    endTime: '18:00',
+    description: '분기별 프로젝트 마감',
+    location: '사무실',
+    category: '업무',
+    repeat: { type: 'none', interval: 0 },
+    notificationTime: 30,
+  },
+];
+
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    expect(getUpcomingEvents(events, new Date(2024, 9, 25, 8, 30), [])).toEqual([events[0]]);
+  });
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    expect(getUpcomingEvents(events, new Date(2024, 9, 25, 8, 45), [events[0].id])).toEqual([]);
+  });
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    expect(getUpcomingEvents(events, new Date(2024, 9, 25, 8), [])).toEqual([]);
+  });
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    expect(getUpcomingEvents(events, new Date(2024, 9, 25, 10), [])).toEqual([]);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  const events: Event[] = [
+    {
+      id: 'da3ca408-836a-4d98-b67a-ca389d07552b',
+      title: '프로젝트 마감',
+      date: '2024-10-25',
+      startTime: '09:00',
+      endTime: '18:00',
+      description: '분기별 프로젝트 마감',
+      location: '사무실',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 30,
+    },
+  ];
+
+  it('몇 분 후에 어떤 이벤트가 시작되는지 알려주는 메시지를 반환한다.', () => {
+    expect(createNotificationMessage(events[0])).toBe(
+      `${events[0].notificationTime}분 후 ${events[0].title} 일정이 시작됩니다.`
+    );
+  });
 });
