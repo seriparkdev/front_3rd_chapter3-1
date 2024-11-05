af1b7917d16888d81e8b1df37cef7558e1529fe3
seriparkdev <parkseridev@gmail.com>
2024-11-05 23:55:00 +0900

    test: fetchHolidays util test 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,18 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    expect(fetchHolidays(new Date(2024, 11))).toStrictEqual({ '2024-12-25': '크리스마스' });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    expect(fetchHolidays(new Date(2024, 10))).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    expect(fetchHolidays(new Date(2024, 9))).toEqual({
+      '2024-10-03': '개천절',
+      '2024-10-09': '한글날',
+    });
+  });
 });
